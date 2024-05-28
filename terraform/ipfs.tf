a9633cc6d953f7604cba5f42696c42e6e1628827
raghu1306 <140365733+raghu1306@users.noreply.github.com>
2024-05-28 12:27:30 +0530

    Update ipfs.tf

--- terraform/ipfs.tf
+++ terraform/ipfs.tf
@@ -2,7 +2,7 @@ provider "local" {}
 
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "git clone https://github.com/raghu1306/ipfs-demo.git ; cd ipfs-demo ; helm install ipfs-release helm-chart ;aws eks update-kubeconfig --region ${var.region} --name ${var.clusterName} ;  cd .. ; rm -rf ipfs-demo  }
+    command = "git clone https://github.com/raghu1306/ipfs-demo.git ; cd ipfs-demo ; helm install ipfs-release helm-chart ; aws eks update-kubeconfig --region ${var.region} --name ${var.clusterName} ;  cd .. ; rm -rf ipfs-demo  }
 
 
 }
