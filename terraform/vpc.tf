f895ba7970b1e886b599388a79b4dfc9fd0dbbae
raghu1306 <140365733+raghu1306@users.noreply.github.com>
2024-05-28 15:43:38 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
 
-  name = "vprofile-eks"
+  name = "raghu-eks-2"
 
   cidr = "172.20.0.0/16"
   azs = slice(data.aws_availability_zones.available.names, 0, 3)
