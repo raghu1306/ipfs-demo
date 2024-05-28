78e42f24696dea812269fa4e9b893f5ca25af9c2
raghu1306 <140365733+raghu1306@users.noreply.github.com>
2024-05-28 13:54:16 +0530

    Update ipfs.tf

--- terraform/ipfs.tf
+++ terraform/ipfs.tf
@@ -1,7 +1,10 @@
 provider "local" {}
 
+resource "null_resource" "wait_for_node_group" {
+  depends_on = [module.eks]
+
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "git clone https://github.com/raghu1306/ipfs-demo.git ; cd ipfs-demo ; helm install ipfs-release helm-chart ; aws eks update-kubeconfig --region us-east-1 --name raghu-eks" 
+    command = "git clone https://github.com/raghu1306/ipfs-demo.git ; cd ipfs-demo ; helm install ipfs-release helm-chart ; aws eks update-kubeconfig --region ${var.region} --name ${var.clusterName}" 
 }
 }
